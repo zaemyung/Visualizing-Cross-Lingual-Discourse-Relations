87818ae9b7345591f7cdd3a85b26abb9d17f8ca5
zaemyung <zaemyung@gmail.com>
2021-09-28 10:41:45 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.header('A placeholder for the demo of "Visualizing Cross-Lingual Discourse Relations in Multilingual TED Corpora" at CODI 2021 @ EMNLP 2021')
+st.write('The page will be updated soon!')
